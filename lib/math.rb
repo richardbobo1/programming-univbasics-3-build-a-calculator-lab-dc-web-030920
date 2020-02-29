370658074910b9e9acb5576cb3753b31530838a2
Richard Bobo <richard.bobo@gmail.com>
2020-02-29 15:53:59 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -10,11 +10,11 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  divide = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  product = num1*num2
 end
 
 def modulo(num1, num2)
